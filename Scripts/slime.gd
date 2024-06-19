611864ccdaccb04b842d3d16e6749b430ccc6fdb
Ben Donet <ben.donet@outlook.com>
2024-06-19 14:04:06 -0500

    Changed slime to obey gravity

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,14 +1,20 @@
-extends Node2D
+extends CharacterBody2D
 
 const SPEED = 50
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
 	if ray_cast_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
@@ -16,4 +22,12 @@ func _process(delta):
 		direction = 1
 		animated_sprite.flip_h = false
 		
-	position.x += direction * SPEED * delta
+	# Move the player
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+	# position.x += direction * SPEED * delta
