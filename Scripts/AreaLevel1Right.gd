d49b883daf8832d30a71c3c25a612e517b151d3e
Ben Donet <brjd345@gmail.com>
2024-08-05 18:10:33 -0500

    Removed climbing collision display
    Now that the climbing works well, there is no need to continue
    displaying the collision boxes. I can always revert this commit if they
    start acting up again.

--- Scripts/AreaLevel1Right.gd
+++ Scripts/AreaLevel1Right.gd
@@ -1,13 +1,10 @@
 extends Area2D
 
 var isColliding = false
-@onready var collision_display = $CollisionDisplay
 
 func _on_body_entered(body):
 	isColliding = true
-	$CollisionDisplay.visible = !isColliding
 	
 
 func _on_body_exited(body):
 	isColliding = false
-	$CollisionDisplay.visible = !isColliding
