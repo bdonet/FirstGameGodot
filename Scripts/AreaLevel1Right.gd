6b6ff6b350c8b390e1ceedfda68ba8178ce7a87b
Ben Donet <brjd345@gmail.com>
2024-08-05 14:08:35 -0500

    Added visual indicators of climb boxes
    Helps me debug the climbing

--- Scripts/AreaLevel1Right.gd
+++ Scripts/AreaLevel1Right.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 var isColliding = false
+@onready var collision_display = $CollisionDisplay
 
 func _on_body_entered(body):
 	isColliding = true
+	$CollisionDisplay.visible = !isColliding
 	
 
 func _on_body_exited(body):
 	isColliding = false
+	$CollisionDisplay.visible = !isColliding
