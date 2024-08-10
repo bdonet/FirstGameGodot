484d25a9df572ce6652e2144e6b45b41425aa031
Ben Donet <brjd345@gmail.com>
2024-08-09 21:50:31 -0500

    Moved score display to game window overlay

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,8 +1,18 @@
 extends Node
 
 var score = 0
-@onready var score_label = $ScoreLabel
+@onready var score_label = $"../CanvasLayer/ScoreLabel"
+@onready var coins = $"../Coins"
+@onready var maxScore = coins.get_child_count()
 
 func add_point():
 	score += 1
-	score_label.text = "Coins:\n" + str(score) + "/21"
+	setScoreLabelText()
+
+
+func _on_ready():
+	setScoreLabelText()
+
+
+func setScoreLabelText():
+	score_label.text = "Coins:\n" + str(score) + "/" + str(maxScore)
