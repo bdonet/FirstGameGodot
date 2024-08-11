2d642fbdd42f8849f8f1d74f413bc9f3774e1f9c
Ben Donet <brjd345@gmail.com>
2024-08-10 19:50:17 -0500

    Changed advanced slime to have predictable attack run at player
    The unpredictability of the random speed was way too punishing for the
    player, especially after so much map. This change keeps the advanced
    slime very difficult to pass but is more predictable and therefore more
    avoidable.

--- Scripts/advancedslime.gd
+++ Scripts/advancedslime.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 150
+const ATTACK_SPEED = 125
+const PATROL_SPEED = 50
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,14 +11,19 @@ var direction = 1
 @onready var ray_cast_wall_left = $RayCastWallLeft
 @onready var ray_cast_ledge_right = $RayCastLedgeRight
 @onready var ray_cast_ledge_left = $RayCastLedgeLeft
+@onready var scout_ray_cast_right = $ScoutRayCastRight
+@onready var scout_ray_cast_left = $ScoutRayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var timer = $Timer
+
+var speed = 0
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
+	# Turn when facing a wall or ledge
 	if ray_cast_wall_right.is_colliding() or !ray_cast_ledge_right.is_colliding():
 		direction = -1
 		animated_sprite.flip_h = true
@@ -25,16 +31,17 @@ func _physics_process(delta):
 		direction = 1
 		animated_sprite.flip_h = false
 		
-	# Move the player
+	# Check for a player nearby
+	if (scout_ray_cast_left.is_colliding() and direction < 0 or scout_ray_cast_right.is_colliding() and direction > 0):
+		speed = ATTACK_SPEED
+	else:
+		speed = PATROL_SPEED
+	
+	# Move the slime
 	if direction:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	
-var speed = 0
 
-func _on_timer_timeout():
-	speed = randi() % (MAX_SPEED + 1)
-	timer.start()
