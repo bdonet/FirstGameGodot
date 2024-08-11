8d959e090bef1d4a868e5fc073c4e070060d7ae4
Ben Donet <brjd345@gmail.com>
2024-08-10 19:07:01 -0500

    Reverted unintentional changes to coin script

--- Scripts/apple.gd
+++ Scripts/apple.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-@onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
+signal pickup
+
+
 func _on_body_entered(_body):
-	game_manager.add_point()
+	pickup.emit()
 	animation_player.play("pickup")
