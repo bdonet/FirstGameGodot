d2d28959ae6d16550f23325e4b1c787aeb6f730d
Ben Donet <brjd345@gmail.com>
2024-10-28 17:30:51 -0500

    Added death counter to screen
    Displays the number of deaths during the game

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,14 +2,17 @@ extends Node
 
 var score = 0
 var apples = 0
-@onready var score_label = $"../CanvasLayer/MarginContainer/ScoreLabel"
+var deaths = 0
+
+@onready var score_label = $"../CanvasLayer/RightScoreboard/ScoreLabel"
 @onready var coins = $"../Coins"
 @onready var maxScore = coins.get_child_count()
-@onready var apple_scoreboard = $"../CanvasLayer/MarginContainer/ScoreLabel/AppleControl/AppleImage"
+@onready var apple_scoreboard = $"../CanvasLayer/RightScoreboard/ScoreLabel/AppleControl/AppleImage"
 @onready var score_summary = $"../Labels/ScoreSummary"
 @onready var death_timer = $DeathTimer
 @onready var player = $"../Player"
 @onready var apple = $"../Apple"
+@onready var death_label = $"../CanvasLayer/LeftScoreboard/DeathLabel"
 
 func add_point():
 	score += 1
@@ -23,6 +26,9 @@ func _on_ready():
 func setScoreLabelText():
 	score_label.text = "x" + str(score)
 	score_summary.text = "Coins: " + str(score) + "/" + str(maxScore) + "\nApples: " + str(apples) + "/1"
+	
+func setDeathLabelText():
+	death_label.text = "x" + str(deaths)
 
 
 func _on_apple_pickup():
@@ -34,17 +40,26 @@ func _on_coins_pickup():
 	add_point()
 
 func _on_player_died():
-	print("You died!")
+	# Wait a bit for a death scene
 	Engine.time_scale = 0.5
-	player.isDead = true
 	death_timer.start()
+	
+	print("You died!")
+	player.isDead = true
+	deaths += 1
+	setDeathLabelText()
+	
+	# Reset the apple if the player had it
 	apples = 0
 	apple_scoreboard.visible = false
 	setScoreLabelText()
 	apple.reset()
 
 func _on_death_timer_timeout():
+	# Make time move normally again
 	Engine.time_scale = 1
+	
+	# Reset player
 	player.position.x = player.startX
 	player.position.y = player.startY
 	player.isDead = false
