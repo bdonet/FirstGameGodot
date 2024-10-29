ec799b492fe24b09759ffda3fa43eb4a50fc1022
Ben Donet <brjd345@gmail.com>
2024-10-29 11:29:43 -0500

    Made apple platform wait until player lands on it
    The platform was moving too soon, so it was possible for the player to
    miss it. The platform now waits for the player to land before moving and
    freezing the player.

--- Scripts/apple_platform.gd
+++ Scripts/apple_platform.gd
@@ -2,6 +2,8 @@ extends AnimatableBody2D
 
 signal player_boarded
 
+var player_hit_upper = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,5 +13,10 @@ func _process(_delta):
 	pass
 
 
-func _on_player_detection_area_player_entered():
-	player_boarded.emit()
+func _on_upper_player_detection_area_player_entered():
+	player_hit_upper = true
+
+
+func _on_lower_player_detection_area_player_entered():
+	if (player_hit_upper):
+		player_boarded.emit()
