ea5c09fcb641c7d92dedb6d922f62c6e4cc36f77
Ben Donet <brjd345@gmail.com>
2024-11-02 15:17:22 -0500

    Rebuilt player movement to use object reflection
    Just like the collision indicator, the detectors for coyote jump and
    climbing were not reflecting. I made them reflect now, so jumping should
    be more efficient and consistent

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -15,21 +15,15 @@ var start_position
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var coyote_ray_cast_right = $CoyoteRayCastRight
-@onready var coyote_ray_cast_left = $CoyoteRayCastLeft
+@onready var coyote_ray_cast = $CoyoteRayCast
 @onready var jump_save_ray_cast = $JumpSaveRayCast
-@onready var area_level_1_right = $AreaLevel1Right
-@onready var area_level_1_left = $AreaLevel1Left
-@onready var area_level_2_right = $AreaLevel2Right
-@onready var area_level_2_left = $AreaLevel2Left
-@onready var area_level_3_right = $AreaLevel3Right
-@onready var area_level_3_left = $AreaLevel3Left
-@onready var area_level_1_right_save = $AreaLevel1RightSave
-@onready var area_level_1_left_save = $AreaLevel1LeftSave
-@onready var area_level_2_right_save = $AreaLevel2RightSave
-@onready var area_level_2_left_save = $AreaLevel2LeftSave
-@onready var area_level_3_right_save = $AreaLevel3RightSave
-@onready var area_level_3_left_save = $AreaLevel3LeftSave
+@onready var area_level_1 = $AreaLevel1
+@onready var area_level_2 = $AreaLevel2
+@onready var area_level_3 = $AreaLevel3
+@onready var area_level_1_save = $AreaLevel1Save
+@onready var area_level_2_save = $AreaLevel2Save
+@onready var area_level_3_save = $AreaLevel3Save
+
 @onready var climb_indicator = $ClimbIndicator
 
 # Save the starting position so we know where to reset to on death	
@@ -83,7 +77,7 @@ func _physics_process(delta):
 		else:
 			animated_sprite.play("jump")
 				# Handle jump.
-		if (Input.is_action_just_pressed("jump") or jumpSaved) and (is_on_floor() or (coyote_ray_cast_left.is_colliding() and direction > 0) or (coyote_ray_cast_right.is_colliding() and direction < 0)):
+		if (Input.is_action_just_pressed("jump") or jumpSaved) and (is_on_floor() or (coyote_ray_cast.is_colliding())):
 			velocity.y = JUMP_VELOCITY
 			jumpSaved = false
 			
@@ -92,46 +86,52 @@ func _physics_process(delta):
 			jumpSaved = true
 			
 		# Handle climb
-		if Input.is_action_just_pressed("jump") or climbSaved == ClimbType.SaveRight or climbSaved == ClimbType.SaveLeft:
+		if Input.is_action_just_pressed("jump") or climbSaved == ClimbType.Save:
 			jumpSaved = false
 			var climbType = GetClimbType()
-			if climbType == ClimbType.RightLow:
+			if climbType == ClimbType.Low:
 				velocity.y = LOW_CLIMB_VELOCITY
 				climbSaved = ClimbType.None
-			elif climbType == ClimbType.LeftLow:
-				velocity.y = LOW_CLIMB_VELOCITY
-				climbSaved = ClimbType.None
-			elif climbType == ClimbType.RightHigh:
-				velocity.y = HIGH_CLIMB_VELOCITY
-				climbSaved = ClimbType.None
-			elif climbType == ClimbType.LeftHigh:
+			elif climbType == ClimbType.High:
 				velocity.y = HIGH_CLIMB_VELOCITY
 				climbSaved = ClimbType.None
 				
 		# Handle climb just before valid climbing
-		if Input.is_action_just_pressed("jump") and !is_on_floor() and (GetClimbType() == ClimbType.SaveRight or GetClimbType() == ClimbType.SaveLeft):
+		if Input.is_action_just_pressed("jump") and !is_on_floor() and (GetClimbType() == ClimbType.Save):
 			climbSaved = GetClimbType()
 		
 		# Reset climb saving when on ground
 		if is_on_floor():
 			climbSaved = ClimbType.None
 			
-		# Flip the sprite to face the current direction
+		# Flip the sprites and movement objects to face the current direction
 		if direction > 0:
 			animated_sprite.flip_h = false
+			climb_indicator.offset.x = abs(climb_indicator.offset.x)
+			coyote_ray_cast.target_position.x = -abs(coyote_ray_cast.target_position.x)
+			area_level_1.position.x = abs(area_level_1.position.x)
+			area_level_2.position.x = abs(area_level_2.position.x)
+			area_level_3.position.x = abs(area_level_3.position.x)
+			area_level_1_save.position.x = abs(area_level_1_save.position.x)
+			area_level_2_save.position.x = abs(area_level_2_save.position.x)
+			area_level_3_save.position.x = abs(area_level_3_save.position.x)
 		elif direction < 0:
 			animated_sprite.flip_h = true
+			climb_indicator.offset.x = -abs(climb_indicator.offset.x)		
+			coyote_ray_cast.target_position.x = abs(coyote_ray_cast.target_position.x)
+			area_level_1.position.x = -abs(area_level_1.position.x)
+			area_level_2.position.x = -abs(area_level_2.position.x)
+			area_level_3.position.x = -abs(area_level_3.position.x)
+			area_level_1_save.position.x = -abs(area_level_1_save.position.x)
+			area_level_2_save.position.x = -abs(area_level_2_save.position.x)
+			area_level_3_save.position.x = -abs(area_level_3_save.position.x)
 			
 		# Display an indicator if a climb is possible
 		var currentClimbType = GetClimbType()
-		if (currentClimbType == ClimbType.None or currentClimbType == ClimbType.SaveRight or currentClimbType == ClimbType.SaveLeft):
+		if (currentClimbType == ClimbType.None or currentClimbType == ClimbType.Save):
 			climb_indicator.visible = false
 		else:
 			climb_indicator.visible = true
-			if (currentClimbType == ClimbType.RightLow or currentClimbType == ClimbType.RightHigh):
-				climb_indicator.offset.x = abs(climb_indicator.offset.x)
-			else:
-				climb_indicator.offset.x = -abs(climb_indicator.offset.x)
 		
 		# Move the player
 		if direction:
@@ -144,26 +144,17 @@ func _physics_process(delta):
 	
 enum ClimbType {
 	None,
-	RightLow,
-	LeftLow,
-	RightHigh,
-	LeftHigh,
-	SaveRight,
-	SaveLeft
+	Low,
+	High,
+	Save
 }
 	
 func GetClimbType():
-	if area_level_1_right.isColliding and !area_level_2_right.isColliding and animated_sprite.flip_h == false:
-		return ClimbType.RightLow
-	elif area_level_1_left.isColliding and !area_level_2_left.isColliding and animated_sprite.flip_h == true:
-		return ClimbType.LeftLow
-	elif area_level_2_right.isColliding and !area_level_3_right.isColliding and animated_sprite.flip_h == false:
-		return ClimbType.RightHigh
-	elif area_level_2_left.isColliding and !area_level_3_left.isColliding and animated_sprite.flip_h == true:
-		return ClimbType.LeftHigh
-	elif area_level_1_right_save.isColliding or area_level_2_right_save.isColliding or area_level_3_right_save.isColliding:
-		return ClimbType.SaveRight
-	elif area_level_1_left_save.isColliding or area_level_2_left_save.isColliding or area_level_3_left_save.isColliding:
-		return ClimbType.SaveLeft
+	if area_level_1.isColliding and !area_level_2.isColliding:
+		return ClimbType.Low
+	elif area_level_2.isColliding and !area_level_3.isColliding:
+		return ClimbType.High
+	elif area_level_1_save.isColliding or area_level_2_save.isColliding or area_level_3_save.isColliding:
+		return ClimbType.Save
 		
 	return ClimbType.None
