f3160003eb16e0fee95d029d803c63685ab48fd9
Ben Donet <brjd345@gmail.com>
2025-04-01 07:38:50 -0500

    Froze basic slime when stunned

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -12,27 +12,28 @@ var direction = 1
 
 signal player_died
 
+var canMove = true
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if ray_cast_wall.is_colliding() or !ray_cast_ledge.is_colliding():
-		direction *= -1
-		animated_sprite.flip_h = not animated_sprite.flip_h
-		ray_cast_wall.target_position.x *= -1
-		ray_cast_ledge.target_position.x *= -1
-		ray_cast_ledge.position.x *= -1
-		
-	# Move the player
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if (canMove):
+		if ray_cast_wall.is_colliding() or !ray_cast_ledge.is_colliding():
+			direction *= -1
+			animated_sprite.flip_h = not animated_sprite.flip_h
+			ray_cast_wall.target_position.x *= -1
+			ray_cast_ledge.target_position.x *= -1
+			ray_cast_ledge.position.x *= -1
+			
+		# Move the player
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-	
-	# position.x += direction * SPEED * delta
 
 
 func _on_killzone_player_died():
@@ -41,4 +42,10 @@ func _on_killzone_player_died():
 
 func hit():
 	print("Slime was hit!")
+	freeze()
 	animated_sprite.play("stun")
+
+
+func freeze():
+	canMove = false
+	velocity.x = 0
