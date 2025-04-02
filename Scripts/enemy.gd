f61c60fb767b7499d6cc4ff22f17adc47a7b7d31
Ben Donet <brjd345@gmail.com>
2025-04-02 18:03:48 -0500

    Refactored slime code to be able to reuse it for all enemies
    Ex. All enemies need to be stunnable, so fhis code should be reusable
    through inheritance.
    
    I still need to refactor the advanced slime to be attackable and extend
    the parent enemy scene

---
+++ Scripts/enemy.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+class_name Enemy
+
+
+const SPEED = 50
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var stun_timer = $StunTimer
+
+signal player_died
+
+var canMove = true
+var canAttack = true
+
+
+func _on_killzone_player_died():
+	if (canAttack):
+		player_died.emit()
+
+
+func stun():
+	freeze()
+	stun_timer.start()
+
+
+func freeze():
+	canMove = false
+	canAttack = false
+
+
+func unfreeze():
+	canMove = true
+	canAttack = true
+
+
+func _on_stun_timer_timeout():
+	unfreeze()
--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -1,20 +1,12 @@
-extends CharacterBody2D
+extends Enemy
 
-const SPEED = 50
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 1
 
 @onready var ray_cast_wall = $RayCastWall
 @onready var ray_cast_ledge = $RayCastLedge
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var stun_timer = $StunTimer
 
-signal player_died
-
-var canMove = true
-var canAttack = true
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -38,28 +30,16 @@ func _physics_process(delta):
 	move_and_slide()
 
 
-func _on_killzone_player_died():
-	if (canAttack):
-		player_died.emit()
-
-
 func stun():
-	freeze()
-	stun_timer.start()
 	animated_sprite.play("stun")
+	super.stun()
 
 
 func freeze():
-	canMove = false
-	canAttack = false
 	velocity.x = 0
+	super.freeze()
 
 
 func unfreeze():
-	canMove = true
-	canAttack = true
-
-
-func _on_stun_timer_timeout():
-	unfreeze()
 	animated_sprite.play("default")
+	super.unfreeze()
